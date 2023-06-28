7ed8917bbe4812b5a58974a273a0ff33420b4603
marigabirodcue <rodriguezcmariaga@gmail.com>
2023-06-27 21:59:27 -0400

    Implementación del Layout

---
+++ src/Components/Layout/index.jsx
@@ -0,0 +1,23 @@
+/*const Layout = ({ children }) => {
+    return (
+        <div className='flex flex-col items-center mt-20'>
+            {children}
+        </div>
+    )
+}*/
+
+import PropTypes from 'prop-types'
+
+const Layout = ({ children }) => {
+    Layout.propTypes = {
+        children: PropTypes.node.isRequired,
+    }
+
+    return (
+        <div className='flex flex-col items-center mt-20'>
+            {children}
+        </div>
+    )
+}
+
+export default Layout
