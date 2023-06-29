09a06a29e420e4caea8a6ab97531899b388a07a3
marigabirodcue <rodriguezcmariaga@gmail.com>
2023-06-29 07:22:44 -0400

    Maquetando el ProductDetail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -21,16 +21,30 @@ import { createContext, useState } from 'react'
 export const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({ children }) => {
+    // Shopping Cart · Increment quantity
     const [count, setCount] = useState(0);
 
     ShoppingCartProvider.propTypes = {
         children: PropTypes.node.isRequired,
     }
 
+    // Product Detail · Open/Close
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const closeProductDetail = () => setIsProductDetailOpen(false)
+
+    // Product Detail · Show product
+    const [productToShow, setProductToShow] = useState({})
+
     return (
         <ShoppingCartContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>
