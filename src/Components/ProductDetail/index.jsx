cc02adc45caf1a3c6ed9871fbe10585e53ea4736
marigabirodcue <rodriguezcmariaga@gmail.com>
2023-06-29 09:01:23 -0400

    Agregando productos al carrito

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -5,7 +5,7 @@ import './styles.css'
 
 const ProductDetail = () => {
     const context = useContext(ShoppingCartContext);
-    console.log('PRODUCT TO SHOW: ', context.productToShow);
+    //console.log('PRODUCT TO SHOW: ', context.productToShow);
 
     return (
         <aside
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -21,20 +21,25 @@ import { createContext, useState } from 'react'
 export const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({ children }) => {
-    // Shopping Cart · Increment quantity
-    const [count, setCount] = useState(0);
 
     ShoppingCartProvider.propTypes = {
         children: PropTypes.node.isRequired,
     }
+
+    // Shopping Cart · Increment quantity
+    const [count, setCount] = useState(0);
 
     // Product Detail · Open/Close
     const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
     const openProductDetail = () => setIsProductDetailOpen(true)
     const closeProductDetail = () => setIsProductDetailOpen(false)
 
+
     // Product Detail · Show product
     const [productToShow, setProductToShow] = useState({})
+
+    // Shopping Cart · Add products to cart
+    const [cartProducts, setCartProducts] = useState([])
 
     return (
         <ShoppingCartContext.Provider value={{
@@ -44,7 +49,9 @@ export const ShoppingCartProvider = ({ children }) => {
             closeProductDetail,
             isProductDetailOpen,
             productToShow,
-            setProductToShow
+            setProductToShow,
+            cartProducts,
+            setCartProducts
         }}>
             {children}
         </ShoppingCartContext.Provider>
