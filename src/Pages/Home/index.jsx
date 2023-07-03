11c0b4718dd4fbd56d325db1bd7ce6ea18b8a5be
marigabirodcue <rodriguezcmariaga@gmail.com>
2023-07-03 18:55:33 -0400

    Implementar las rutas del Navbar para filtrar por categorías

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -8,24 +8,16 @@ function Home() {
     const context = useContext(ShoppingCartContext)
 
     const renderView = () => {
-        if (context.searchByTitle?.length > 0) {
-            if (context.filteredItems?.length > 0) {
-                return (
-                    context.filteredItems?.map(item => (
-                        <Card key={item.id} data={item} />
-                    ))
-                )
-            } else {
-                return (
-                    // eslint-disable-next-line react/no-unescaped-entities
-                    <div>We don't have anything :(</div>
-                )
-            }
-        } else {
+        if (context.filteredItems?.length > 0) {
             return (
-                context.items?.map(item => (
+                context.filteredItems?.map(item => (
                     <Card key={item.id} data={item} />
                 ))
+            )
+        } else {
+            return (
+                // eslint-disable-next-line react/no-unescaped-entities
+                <div>We don't have anything :(</div>
             )
         }
     }
